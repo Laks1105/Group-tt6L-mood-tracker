3db3af72b20f5f5dd8cbbe4fa18f5b35a5718997
Lakshme <pblaks1105@gmail.com>
2025-06-07 21:24:06 +0800

    added line

--- login_register.py
+++ login_register.py
@@ -251,4 +251,6 @@ def shuffle_quote():
 if __name__ == '__main__':
     if not os.path.exists('user_id_password.db'):
         init_db()
+        port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
