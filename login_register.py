395ca54d0d6fc6647f497bf2ea8c85cfdc56837f
Lakshme <pblaks1105@gmail.com>
2025-06-17 14:04:10 +0800

    added line

--- login_register.py
+++ login_register.py
@@ -305,20 +305,32 @@ def change_password():
             db.session.commit()
     return redirect(url_for('settings'))
 
-#Delete Account
+
 @app.route('/delete_account', methods=['POST'])
 def delete_account():
     user_id = session.get('user_id') #checking which user_id is used to login
 
     if user_id:
-        user = User.query.get(user_id)
-        if user:
-            db.session.delete(user)
-            db.session.commit()
-        session.clear()
-        return redirect(url_for('login')) #get back to the login page once the user db is deleted
+        try:
+            conn = sqlite3.connect('user_id_password.db')
+            cursor = conn.cursor()
 
-    return render_template('settings_1.html', error="User not found.") 
+            cursor.execute('SELECT * FROM users')
+            print("All users:", cursor.fetchall())
+
+            cursor.execute('DELETE FROM users WHERE id = ?', (user_id,))
+            conn.commit()
+            print("Account deleted.")
+
+            session.clear()
+            conn.close()
+            return redirect(url_for('login'))  #get back to the login page once the user db is deleted
+
+        except Exception as e:
+            print("Error:", e)
+            return render_template('settings_1.html')  
+
+    return render_template('settings_1.html')
 
 
 if __name__ == '__main__': 
